930220521392b23c08263580f7d273bbf308165c
Diego Andres Niño Rendon <daninorendon@gmail.com>
2024-12-27 19:24:38 -0500

    fixing and osh logo

--- js/font.js
+++ js/font.js
@@ -1,18 +1,21 @@
 let paper
 let links
 let countdown
+let cuenta
 document.addEventListener("DOMContentLoaded", function() {
   paper = document.getElementById("paper")
   countdown = document.getElementById("countdown")
+  cuenta = document.getElementById("cuenta")
   links = document.querySelectorAll(".link")
   links.forEach((link) => {
-    link.style.fontSize = `${Math.round(paper.offsetWidth * 0.020)}px`
+    link.style.fontSize = `${Math.round(paper.offsetWidth * 0.025)}px`
   })
-  countdown.style.fontSize = `${Math.round(paper.offsetWidth * 0.025)}px`
+  countdown.style.fontSize = `${Math.round(paper.offsetWidth * 0.035)}px`
+  cuenta.style.fontSize = `${Math.round(paper.offsetWidth * 0.030)}px`
 })
 window.addEventListener("resize", function() {
   links.forEach((link) => {
-    link.style.fontSize = `${Math.round(paper.offsetWidth * 0.020)}px`
-    countdown.style.fontSize = `${Math.round(paper.offsetWidth * 0.025)}px`
+    link.style.fontSize = `${Math.round(paper.offsetWidth * 0.025)}px`
+    countdown.style.fontSize = `${Math.round(paper.offsetWidth * 0.030)}px`
   })
 })
