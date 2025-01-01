7af6958a7f5987b2ba9473cfa1c5c6899ec5d420
Diego Andres Niño Rendon <daninorendon@gmail.com>
2025-01-01 12:36:50 -0500

    numeric codes

--- js/asistencia.js
+++ js/asistencia.js
@@ -2,12 +2,12 @@ function acceder() {
   let input = document.getElementById("asistenciaInput")
   let codigo = input.value
   switch (codigo) {
-    case "2dd072ae-cf9d":
+    case "011440486217":
       window.open("https://forms.gle/sJNXFbibifcveZow6")
       input.style.borderColor = "green"
       break;
 
-    case "a97264d9-811a":
+    case "091354566426":
       window.open("https://forms.gle/D1nRd4stfLGJK9nT7")
       input.style.borderColor = "green"
       break;
